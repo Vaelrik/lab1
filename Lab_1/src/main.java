dc9add48c21e4b10e8905ac0c448b52e6229ea2a
Vaelrik <sergey2k2k@gmail.com>
2024-04-29 14:01:59 +0700

    Заного

--- Lab_1/src/main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        rectangle.rectangle myRectangle = new rectangle(5, 10); // Creating a new rectangle object with width 5 and height 10
-
-        System.out.println("Периметр: " + myRectangle.getPerimeter());
-        System.out.println("Площадь: " + myRectangle.getArea());
-        System.out.println("Длина диагонали: " + myRectangle.getLenDiagonal());
-    }
-}
